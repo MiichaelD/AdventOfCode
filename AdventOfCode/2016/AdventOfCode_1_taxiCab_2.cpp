78bcae329d72397e6cf7e8eb4d8c5f490c8e998c
Michael Duarte <michael.duarte@hotmail.com>
2016-12-01 22:24:48 -0800

    fix indentation, switched from tabs to spaces.

--- AdventOfCode/2016/AdventOfCode_1_taxiCab_2.cpp
+++ AdventOfCode/2016/AdventOfCode_1_taxiCab_2.cpp
@@ -1,26 +1,26 @@
 /*
-	Description: 	The Document indicates that you should start at the given
-								coordinates (where you just landed) and face North. Then, follow
-								the provided sequence: either turn left (L) or right (R) 90
-								degrees, then walk forward the given number of blocks, ending at
-								a new intersection.
+  Description:  The Document indicates that you should start at the given
+                coordinates (where you just landed) and face North. Then, follow
+                the provided sequence: either turn left (L) or right (R) 90
+                degrees, then walk forward the given number of blocks, ending at
+                a new intersection.
 
-								There's no time to follow such ridiculous instructions on foot,
-								though, so you take a moment and work out the destination. Given
-								that you can only walk on the street grid of the city, how far
-								is the shortest path to the destination?
+                There's no time to follow such ridiculous instructions on foot,
+                though, so you take a moment and work out the destination. Given
+                that you can only walk on the street grid of the city, how far
+                is the shortest path to the destination?
 
-	Example:			Following R2, L3 leaves you 2 blocks East and 3 blocks North, or
-								5 blocks away.
-								R2, R2, R2 leaves you 2 blocks due South of your starting
-								position, which is 2 blocks away.
-								R5, L5, R5, R3 leaves you 12 blocks away.
-								
-								How many blocks away is Easter Bunny HQ?
+  Example:      Following R2, L3 leaves you 2 blocks East and 3 blocks North, or
+                5 blocks away.
+                R2, R2, R2 leaves you 2 blocks due South of your starting
+                position, which is 2 blocks away.
+                R5, L5, R5, R3 leaves you 12 blocks away.
+                
+                How many blocks away is Easter Bunny HQ?
 
-	Programmer:		Michael Duarte.
+  Programmer:   Michael Duarte.
 
-	Date:					12/01/2016
+  Date:         12/01/2016
 */
 
 #include <iostream>
@@ -42,42 +42,42 @@ struct comma_space_enter : std::ctype<char> {
 };
 
 struct Coordinate {
-	int x = 0, y = 0;
-	
-	Coordinate() = default;
-	
-	Coordinate(int xx, int yy):x(xx),y(yy) { }
+  int x = 0, y = 0;
+  
+  Coordinate() = default;
+  
+  Coordinate(int xx, int yy):x(xx),y(yy) { }
 
-	bool isSame(const Coordinate &coord) const {
-		return x == coord.x && y == coord.y;
-	}
+  bool isSame(const Coordinate &coord) const {
+    return x == coord.x && y == coord.y;
+  }
 };
 
 
 //http://stackoverflow.com/questions/17016175/c-unordered-map-using-a-custom-class-type-as-the-key
 namespace std{
-	template<>
-	struct hash<Coordinate>{
-		// hash function for unordered_set this will return a 
-		// unique number for a point with col < 8: [0 - 7]
-		size_t operator()(const Coordinate &c) const noexcept{
-			return c.x * 10000 + c.y;
-		}
-	};	
+  template<>
+  struct hash<Coordinate>{
+    // hash function for unordered_set this will return a 
+    // unique number for a point with col < 8: [0 - 7]
+    size_t operator()(const Coordinate &c) const noexcept{
+      return c.x * 10000 + c.y;
+    }
+  };  
 }
 
 bool operator==(const Coordinate &c1, const Coordinate &c2) noexcept{
-	return c1.isSame(c2);
+  return c1.isSame(c2);
 }
 
 
 struct Solution {
-	Coordinate coord;
-	bool solved = false;
-	Solution() = default;
-	Solution(const Coordinate &c):solved(true) {
-		coord = c;
-	}
+  Coordinate coord;
+  bool solved = false;
+  Solution() = default;
+  Solution(const Coordinate &c):solved(true) {
+    coord = c;
+  }
 
 };
 
@@ -87,130 +87,130 @@ char curOrientation = 'N';
 unordered_set<Coordinate> coordinates;
 
 void updateOrientation(char dir){
-	// this can be improved as a ciclic list of ints 0 - 3
-	// R adds 1, L removes 1.
-	switch (curOrientation){
-		case 'N':
-			if (dir == 'R') 
-				curOrientation = 'E';
-			else
-				curOrientation = 'W';
-			break;
+  // this can be improved as a ciclic list of ints 0 - 3
+  // R adds 1, L removes 1.
+  switch (curOrientation){
+    case 'N':
+      if (dir == 'R') 
+        curOrientation = 'E';
+      else
+        curOrientation = 'W';
+      break;
 
-		case 'E':
-			if (dir == 'R')
-				curOrientation = 'S';
-			else
-				curOrientation = 'N';
-			break;
+    case 'E':
+      if (dir == 'R')
+        curOrientation = 'S';
+      else
+        curOrientation = 'N';
+      break;
 
-		case 'S':
-			if (dir == 'R')
-				curOrientation = 'W';
-			else
-				curOrientation = 'E';
-			break;
+    case 'S':
+      if (dir == 'R')
+        curOrientation = 'W';
+      else
+        curOrientation = 'E';
+      break;
 
-		case 'W':
-			if (dir == 'R')
-				curOrientation = 'N';
-			else
-				curOrientation = 'S';
-			break;
-	}
+    case 'W':
+      if (dir == 'R')
+        curOrientation = 'N';
+      else
+        curOrientation = 'S';
+      break;
+  }
 }
 
 Solution addSteps(int steps) {
-	switch (curOrientation) {
-		case 'N':
-			for (int i = 0 ; i < steps; ++i){
-				Coordinate coor(hor, ver+i);
-				if (coordinates.find(coor) != coordinates.end())
-					return Solution(coor);
-				coordinates.insert(coor);
-			}
-			ver += steps;
-			break;
+  switch (curOrientation) {
+    case 'N':
+      for (int i = 0 ; i < steps; ++i){
+        Coordinate coor(hor, ver+i);
+        if (coordinates.find(coor) != coordinates.end())
+          return Solution(coor);
+        coordinates.insert(coor);
+      }
+      ver += steps;
+      break;
 
-		case 'E':
-			for (int i = 0 ; i < steps; ++i){
-				Coordinate coor(hor+i, ver);
-				if (coordinates.find(coor) != coordinates.end())
-					return Solution(coor);
-				coordinates.insert(coor);
-			}
-			hor += steps;
-			break;
+    case 'E':
+      for (int i = 0 ; i < steps; ++i){
+        Coordinate coor(hor+i, ver);
+        if (coordinates.find(coor) != coordinates.end())
+          return Solution(coor);
+        coordinates.insert(coor);
+      }
+      hor += steps;
+      break;
 
-		case 'S':
-			for (int i = 0 ; i < steps; ++i){
-				Coordinate coor(hor, ver-i);
-				if (coordinates.find(coor) != coordinates.end())
-					return Solution(coor);
-				coordinates.insert(coor);
-			}
-			ver -= steps;
-			break;
+    case 'S':
+      for (int i = 0 ; i < steps; ++i){
+        Coordinate coor(hor, ver-i);
+        if (coordinates.find(coor) != coordinates.end())
+          return Solution(coor);
+        coordinates.insert(coor);
+      }
+      ver -= steps;
+      break;
 
-		case 'W':
-			for (int i = 0 ; i < steps; ++i){
-				Coordinate coor(hor-i, ver);
-				if (coordinates.find(coor) != coordinates.end())
-					return Solution(coor);
-				coordinates.insert(coor);
-			}
-			hor -= steps;
-			break;
-	} 
-	return Solution{};
+    case 'W':
+      for (int i = 0 ; i < steps; ++i){
+        Coordinate coor(hor-i, ver);
+        if (coordinates.find(coor) != coordinates.end())
+          return Solution(coor);
+        coordinates.insert(coor);
+      }
+      hor -= steps;
+      break;
+  } 
+  return Solution{};
 }
 
 
 bool process(char dir, int steps) {
-	cout << "found: " << dir << ' ' << steps << endl; 
-	updateOrientation(dir);
-	const Solution &sol = addSteps(steps);
-	if (sol.solved){
-		hor = sol.coord.x;
-		ver = sol.coord.y;
-		return true;
-	}
-	return false;
+  cout << "found: " << dir << ' ' << steps << endl; 
+  updateOrientation(dir);
+  const Solution &sol = addSteps(steps);
+  if (sol.solved){
+    hor = sol.coord.x;
+    ver = sol.coord.y;
+    return true;
+  }
+  return false;
 }
 
 int getDistance() {
-	return abs(hor) + abs(ver);
+  return abs(hor) + abs(ver);
 }
 
 void capture() {
-	char direction;
-	int steps;
-	while (std::cin){
-		cin >> direction >> steps;
-		if (process(direction, steps))
-			break;
-	}
-	process(direction == 'L' ? 'R' : 'L', steps);
+  char direction;
+  int steps;
+  while (std::cin){
+    cin >> direction >> steps;
+    if (process(direction, steps))
+      break;
+  }
+  process(direction == 'L' ? 'R' : 'L', steps);
 }
 
 // figure out why this is getting the last instruction
 void capture1() {
-	char direction, lastDirection;
-	int steps, lastSteps;
-	int n = 0;
-	while (std::cin){
-		cin >> direction >> steps;
-		if (n++){
-			process(lastDirection, lastSteps);
-		}
-		lastDirection = direction;
-		lastSteps = steps;
-	}	
+  char direction, lastDirection;
+  int steps, lastSteps;
+  int n = 0;
+  while (std::cin){
+    cin >> direction >> steps;
+    if (n++){
+      process(lastDirection, lastSteps);
+    }
+    lastDirection = direction;
+    lastSteps = steps;
+  } 
 }
 
 int main(){
-	cin.imbue(locale(cin.getloc(), new comma_space_enter));
-	capture1();
-	cout << getDistance() << endl;
-	return 0;
+  cin.imbue(locale(cin.getloc(), new comma_space_enter));
+  capture1();
+  cout << getDistance() << endl;
+  return 0;
 }
--- AdventOfCode/2016/AdventOfCode_2_restroom_2.cpp
+++ AdventOfCode/2016/AdventOfCode_2_restroom_2.cpp
@@ -1,71 +1,71 @@
 /*
-	Description: 	"bathroom codes will no longer be written down. Instead, please
-								memorize and follow the procedure below to access the bathrooms."
+  Description:  "bathroom codes will no longer be written down. Instead, please
+                memorize and follow the procedure below to access the bathrooms."
 
-								The document goes on to explain that each button to be pressed
-								can be found by starting on the previous button and moving to
-								adjacent buttons on the keypad: U moves up, D moves down, L
-								moves left, and R moves right. Each line of instructions
-								corresponds to one button, starting at the previous button (or,
-								for the first line, the "5" button); press whatever button you
-								are on at the end of each line. If a move doesn't lead to a
-								button, ignore it.
+                The document goes on to explain that each button to be pressed
+                can be found by starting on the previous button and moving to
+                adjacent buttons on the keypad: U moves up, D moves down, L
+                moves left, and R moves right. Each line of instructions
+                corresponds to one button, starting at the previous button (or,
+                for the first line, the "5" button); press whatever button you
+                are on at the end of each line. If a move doesn't lead to a
+                button, ignore it.
 
-								You can't hold it much longer, so you decide to figure out the
-								code as you walk to the bathroom. You picture a keypad like this:
+                You can't hold it much longer, so you decide to figure out the
+                code as you walk to the bathroom. You picture a keypad like this:
 
-								1 2 3
-								4 5 6
-								7 8 9
-								Suppose your instructions are:
-								ULL
-								RRDDD
-								LURDL
-								UUUUD
-								- You start at "5" and move up (to "2"), left (to "1"), and left
-								(you can't, and stay on "1"), so the first button is 1.
-								- Starting from the previous button ("1"), you move right twice
-								(to "3") and then down three times (stopping at "9" after two
-								moves and ignoring the third), ending up with 9.
-								- Continuing from "9", you move left, up, right, down, and left,
-								ending with 8.
-								- Finally, you move up four times (stopping at "2"), then down
-								once, ending with 5.
-								
-								So, in this example, the bathroom code is 1985.
+                1 2 3
+                4 5 6
+                7 8 9
+                Suppose your instructions are:
+                ULL
+                RRDDD
+                LURDL
+                UUUUD
+                - You start at "5" and move up (to "2"), left (to "1"), and left
+                (you can't, and stay on "1"), so the first button is 1.
+                - Starting from the previous button ("1"), you move right twice
+                (to "3") and then down three times (stopping at "9" after two
+                moves and ignoring the third), ending up with 9.
+                - Continuing from "9", you move left, up, right, down, and left,
+                ending with 8.
+                - Finally, you move up four times (stopping at "2"), then down
+                once, ending with 5.
+                
+                So, in this example, the bathroom code is 1985.
 
-								You finally arrive at the bathroom (it's a several minute walk
-								from the lobby so visitors can behold the many fancy conference
-								rooms and water coolers on this floor) and go to punch in the
-								code. Much to your bladder's dismay, the keypad is not at all
-								like you imagined it. Instead, you are confronted with the
-								result of hundreds of man-hours of bathroom-keypad-design
-								meetings:
-								    1
-								  2 3 4
-								5 6 7 8 9
-								  A B C
-								    D
+                You finally arrive at the bathroom (it's a several minute walk
+                from the lobby so visitors can behold the many fancy conference
+                rooms and water coolers on this floor) and go to punch in the
+                code. Much to your bladder's dismay, the keypad is not at all
+                like you imagined it. Instead, you are confronted with the
+                result of hundreds of man-hours of bathroom-keypad-design
+                meetings:
+                    1
+                  2 3 4
+                5 6 7 8 9
+                  A B C
+                    D
 
-								You still start at "5" and stop when you're at an edge, but
-								given the same instructions as above, the outcome is very
-								different:
+                You still start at "5" and stop when you're at an edge, but
+                given the same instructions as above, the outcome is very
+                different:
 
-								- You start at "5" and don't move at all (up and left are both
-								edges), ending at 5.
-								- Continuing from "5", you move right twice and down three times
-								(through "6", "7", "B", "D", "D"), ending at D.
-								- Then, from "D", you move five more times (through "D", "B",
-								"C", "C", "B"), ending at B.
-								- Finally, after five more moves, you end at 3.
-								So, given the actual keypad layout, the code would be 5DB3.
+                - You start at "5" and don't move at all (up and left are both
+                edges), ending at 5.
+                - Continuing from "5", you move right twice and down three times
+                (through "6", "7", "B", "D", "D"), ending at D.
+                - Then, from "D", you move five more times (through "D", "B",
+                "C", "C", "B"), ending at B.
+                - Finally, after five more moves, you end at 3.
+                So, given the actual keypad layout, the code would be 5DB3.
 
-								Using the same instructions in your puzzle input, what is the
-								correct bathroom code?
+                Using the same instructions in your puzzle input, what is the
+                correct bathroom code?
 
-	Programmer:		Michael Duarte.
+  Programmer:   Michael Duarte.
 
-	Date:					12/01/2016
+  Date:         12/01/2016
 */
 
 #include <iostream>
@@ -77,96 +77,96 @@ using namespace std;
 vector<vector<char>> keypad(5, vector<char>(5));
 
 void fill() {
-	keypad[0][0] = '_';
-	keypad[0][1] = '_';
-	keypad[0][2] = '5';
-	keypad[0][3] = '_';
-	keypad[0][4] = '_';
+  keypad[0][0] = '_';
+  keypad[0][1] = '_';
+  keypad[0][2] = '5';
+  keypad[0][3] = '_';
+  keypad[0][4] = '_';
 
-	keypad[1][0] = '_';
-	keypad[1][1] = '2';
-	keypad[1][2] = '6';
-	keypad[1][3] = 'A';
-	keypad[1][4] = '_';
+  keypad[1][0] = '_';
+  keypad[1][1] = '2';
+  keypad[1][2] = '6';
+  keypad[1][3] = 'A';
+  keypad[1][4] = '_';
 
-	keypad[2][0] = '1';
-	keypad[2][1] = '3';
-	keypad[2][2] = '7';
-	keypad[2][3] = 'B';
-	keypad[2][4] = 'D';
+  keypad[2][0] = '1';
+  keypad[2][1] = '3';
+  keypad[2][2] = '7';
+  keypad[2][3] = 'B';
+  keypad[2][4] = 'D';
 
-	keypad[3][0] = '_';
-	keypad[3][1] = '4';
-	keypad[3][2] = '8';
-	keypad[3][3] = 'C';
-	keypad[3][4] = '_';
+  keypad[3][0] = '_';
+  keypad[3][1] = '4';
+  keypad[3][2] = '8';
+  keypad[3][3] = 'C';
+  keypad[3][4] = '_';
 
-	keypad[4][0] = '_';
-	keypad[4][1] = '_';
-	keypad[4][2] = '9';
-	keypad[4][3] = '_';
-	keypad[4][4] = '_';
+  keypad[4][0] = '_';
+  keypad[4][1] = '_';
+  keypad[4][2] = '9';
+  keypad[4][3] = '_';
+  keypad[4][4] = '_';
 }
 
 
 bool canUp(int x, int y){
-	if (y < 1 || keypad[x][y-1] == '_')
-		return false;
-	return true;
+  if (y < 1 || keypad[x][y-1] == '_')
+    return false;
+  return true;
 }
 
 bool canDown(int x, int y){
-	if (y > 3 || keypad[x][y+1] == '_')
-		return false;
-	return true;
+  if (y > 3 || keypad[x][y+1] == '_')
+    return false;
+  return true;
 }
 
 bool canLeft(int x, int y){
-	if (x < 1 || keypad[x-1][y] == '_')
-		return false;
-	return true;
+  if (x < 1 || keypad[x-1][y] == '_')
+    return false;
+  return true;
 }
 
 bool canRight(int x, int y){
-	if (x > 3 || keypad[x+1][y] == '_')
-		return false;
-	return true;
+  if (x > 3 || keypad[x+1][y] == '_')
+    return false;
+  return true;
 }
 
 
 int main() {
-	char c;
-	string input;
-	fill();
-	int x = 0, y = 2;
-	cout << "Starting at:\t" << keypad[x][y] << endl; 
-	while(cin){
-		cin >> input;
-		for (int i = 0 ; i < input.length(); ++i) {
-			c = input[i];
-			switch(c) {
-				case 'U':
-					if (canUp(x,y)) --y;
-					break;
+  char c;
+  string input;
+  fill();
+  int x = 0, y = 2;
+  cout << "Starting at:\t" << keypad[x][y] << endl; 
+  while(cin){
+    cin >> input;
+    for (int i = 0 ; i < input.length(); ++i) {
+      c = input[i];
+      switch(c) {
+        case 'U':
+          if (canUp(x,y)) --y;
+          break;
 
-				case 'R':
-					if (canRight(x,y)) ++x;
-					break;
+        case 'R':
+          if (canRight(x,y)) ++x;
+          break;
 
-				case 'D':
-					if (canDown(x,y)) ++y;
-					break;
+        case 'D':
+          if (canDown(x,y)) ++y;
+          break;
 
-				case 'L':
-					if (canLeft(x,y)) --x;
-					break;
+        case 'L':
+          if (canLeft(x,y)) --x;
+          break;
 
-				default:
-					break;
-			};
-			// cout << "\t" << c << ": " << keypad[x][y] << endl; 
-		}
-		cout << keypad[x][y] << endl;
-	}
-	return EXIT_SUCCESS;
+        default:
+          break;
+      };
+      // cout << "\t" << c << ": " << keypad[x][y] << endl; 
+    }
+    cout << keypad[x][y] << endl;
+  }
+  return EXIT_SUCCESS;
 }
