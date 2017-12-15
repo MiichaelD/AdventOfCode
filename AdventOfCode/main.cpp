f590ac5d038abce1eb15cc99ca1704a9cf033286
Michael Duarte <michael.duarte@hotmail.com>
2017-12-14 22:44:31 -0800

    Added 2017/15 - Dueling Generators's solution

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,7 +1,7 @@
 
-#include "2017/AdventOfCode_01_InverseCaptcha.h"
+#include "2017/AdventOfCode_15_DuelingGenerators.h"
 
 int main() {
-  aoc2017_01::solve(2);
+  aoc2017_15::solve(2);
   return EXIT_SUCCESS;
 }
