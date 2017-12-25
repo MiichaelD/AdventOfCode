2050b42d3701d0b65f17ee6d00b0edbe87982e6d
Michael Duarte <michael.duarte@hotmail.com>
2017-12-24 20:58:39 -0800

    Added solutions for day 4.

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,7 +1,7 @@
 
-#include "2017/AdventOfCode_02_corruptionChecksum.h"
+#include "2017/AdventOfCode_04_highEntropyPassphrase.h"
 
 int main() {
-  aoc2017_02::solve(2);
+  aoc2017_04::solve(2);
   return EXIT_SUCCESS;
 }
