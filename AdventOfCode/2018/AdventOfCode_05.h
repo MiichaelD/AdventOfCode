3734932db1cd393421675bae2db4e34d1b02a85f
Michael Duarte <michael.duarte@hotmail.com>
2018-12-27 19:26:31 -0800

    Completes 2018 - 05 part 2

--- AdventOfCode/2018/AdventOfCode_05.h
+++ AdventOfCode/2018/AdventOfCode_05.h
@@ -39,7 +39,9 @@
 #include <string>
 #include <deque>
 #include <cmath>
-#include <unordered_set>
+#include <cctype>
+#include <climits>
+#include <unordered_map>
 #include <vector>
 
 namespace aoc2018_05 {
@@ -49,29 +51,74 @@ namespace aoc2018_05 {
   bool reacts(char a, char b) {
     return abs(a - b) == reactionDiffValue;
   }
-  
-  void solve(int part = 1) {
-    using namespace std;
 
-    reactionDiffValue = 'a' - 'A';
-    cout << "reactionDiffValue: " << reactionDiffValue << endl;
+  bool ignoreChar(char c, char ignoredChar) {
+    int diff = abs(c - ignoredChar);
+    return diff == 0 || diff == reactionDiffValue;
+  }
 
-    string input;
-    deque<char> reducedStack;
-    cin >> input;
-
+  std::deque<char> getReducedInput(const std::string &input, char ignoredChar = ' ') {
+    std::deque<char> reducedStack;
     for (int i = 0; i < input.size(); ++i) {
+      if (ignoreChar(input[i], ignoredChar)) {
+        continue;
+      }
       if (reducedStack.size() > 0 && reacts(reducedStack.back(), input[i])) {
         reducedStack.pop_back();
       } else {
         reducedStack.push_back(input[i]);
       }
     }
+    return reducedStack;
+  }
 
+  void printStack(const std::deque<char> &reducedStack) {
+    using namespace std;
     for (auto it = reducedStack.begin(); it != reducedStack.end(); ++it) {
       cout << *it;
     }
     cout << endl;
+  }
+
+  void solve1(const std::string &input) {
+    printStack(getReducedInput(input));
+  }
+
+  void solve2(const std::string &input) {
+    using namespace std;
+    unordered_map<char,int> reducedMap;
+
+    for (int i = 0; i < input.size(); ++i) {
+      reducedMap.insert(make_pair<char,int>(tolower(input[i]), 0));
+    }
+
+    int shortestLength = INT_MAX;
+    deque<char> smallestReduced;
+    char smallestReducedIgnoredChar;
+    for (auto it = reducedMap.begin(); it != reducedMap.end(); ++it) {
+      deque<char> reducedStack = getReducedInput(input, it->first);
+      it->second = reducedStack.size();
+      if (it->second < shortestLength) {
+        smallestReduced = reducedStack;
+        smallestReducedIgnoredChar = it->first;
+        shortestLength = it->second;
+      }
+      cout << "W/o char: " << it->first << " => " << reducedStack.size() << endl; //": ";
+      // printStack(reducedStack);
+
+    }
+    cout << endl << "Smallest reduced (" << smallestReduced.size() << ") w/o letter: ";
+    cout << smallestReducedIgnoredChar << endl;
+    printStack(smallestReduced);
+  }
+  
+  void solve(int part = 1) {
+    using namespace std;
+
+    reactionDiffValue = 'a' - 'A';
+    string input;
+    cin >> input;
+    if (part == 1) solve1(input); else solve2(input);
   }
 };
 
