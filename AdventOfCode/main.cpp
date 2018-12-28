086ee78cf961276ac03a1b23b2b7d8524cec7011
Michael Duarte <michael.duarte@hotmail.com>
2018-12-27 17:58:30 -0800

    Solves 2018 - 05

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,7 +1,9 @@
 
-#include "2018/AdventOfCode_03.h"
+#include "2018/AdventOfCode_05.h"
+
+//Compiling:    g++ -std=c++11 main.cpp -o main
 
 int main() {
-  aoc2018_03::solve();
+  aoc2018_05::solve();
   return EXIT_SUCCESS;
 }
