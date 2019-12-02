372f74279daf4ec6eb68c9eaf9c2056418745665
Michael Duarte <michael.duarte@hotmail.com>
2019-12-01 19:39:11 -0800

    Adds 1st problem of 2019

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,8 +1,8 @@
 
-#include "2018/AdventOfCode_05.h"
+#include "2019/AdventOfCode_01.h"
 
 // Compiling:    g++ -std=c++11 main.cpp -o main
 int main() {
-  aoc2018_05::solve(2);
+  aoc2019_01::solve(2);
   return EXIT_SUCCESS;
 }
