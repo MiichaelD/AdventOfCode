89b6b47485f88fd823860c9eef3695db716ce297
Michael Duarte <michael.duarte@hotmail.com>
2019-12-03 09:17:57 -0800

    Adds template file

---
+++ AdventOfCode/2019/AdventOfCode_XX.h
@@ -0,0 +1,45 @@
+/*
+  Link:         http://adventofcode.com/2019/day/X
+ 
+
+  Compiling:    g++ -std=c++11 main.cpp -o main
+               
+  Programmer:   Michael Duarte.
+
+  Date:         12/02/2019
+*/
+
+#ifndef _2019_ADVENTOFCODE_XX_H_
+#define _2019_ADVENTOFCODE_XX_H_
+
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+namespace aoc2019_XX {  
+  using namespace std;
+
+  
+
+  void solve1() {   
+    string input;
+    cin >> input;
+    
+  }
+
+  void solve2() {    
+    
+  }
+
+  void solve(int part = 1) {
+    using namespace std;
+    if (part == 1) {
+      solve1();
+    } else {
+      solve2();
+    }
+  }
+};
+
+#endif /* _2019_ADVENTOFCODE_XX_H_ */
