0fa940c1c0ff4698c55889afab9aedb9b2cf88f5
Michael Duarte <michael.duarte@hotmail.com>
2019-12-02 20:48:58 -0800

    Adds day 2

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,8 +1,8 @@
 
-#include "2019/AdventOfCode_01.h"
+#include "2019/AdventOfCode_02.h"
 
 // Compiling:    g++ -std=c++11 main.cpp -o main
 int main() {
-  aoc2019_01::solve(2);
+  aoc2019_02::solve(2);
   return EXIT_SUCCESS;
 }
