6ca94e89b1ef39bce785ebabf02ad4c9665d1d47
Michael Duarte <michael.duarte@hotmail.com>
2019-12-07 11:09:41 -0800

    Adds template for day 8

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,7 +1,7 @@
-#include "2019/AdventOfCode_07.h"
+#include "2019/AdventOfCode_08.h"
 
 // Compiling:    g++ -std=c++11 main.cpp -o main
 int main() {
-  aoc2019_07::solve(2);
+  aoc2019_08::solve(2);
   return EXIT_SUCCESS;
 }
