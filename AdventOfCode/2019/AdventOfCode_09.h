93391ce003ebb2f272df72a955b13fe6c6f81337
Michael Duarte <michael.duarte@hotmail.com>
2019-12-08 21:35:05 -0800

    Adds Day 9 part 1 solution

--- AdventOfCode/2019/AdventOfCode_09.h
+++ AdventOfCode/2019/AdventOfCode_09.h
@@ -23,24 +23,146 @@
 #include <vector>
 
 namespace aoc2019_09 {  
-  using namespace std;
 
-  void solve1() {   
-    string input;
-    cin >> input;
-    
+  using namespace std;
+  
+  const int POS_MODE = 0;
+  const int IMM_MODE = 1;
+  const int REL_MODE = 2;
+
+  long long relativeBase = 0;
+
+  void printIntCodes(const vector<int> &intCodes) {
+    for (int i : intCodes) {
+      cout << i << ", ";
+    }
+    cout << endl;
   }
 
-  void solve2() {    
-    
+  vector<long long> getIntCodes(const string &input) {
+    vector<long long> intCodes;
+    long long aux = 0;
+    bool negative = false;
+    for (int i = 0 ; i < input.length(); ++i) {
+      if (input[i] == '-') {
+        negative = true;
+      } else if (input[i] == ',') {
+        intCodes.push_back(negative ? -aux : aux);
+        negative = false;
+        aux = 0;
+      } else {
+        aux *= 10;
+        aux += (input[i] - '0');
+      }
+    }
+    intCodes.push_back(negative ? -aux : aux);
+    return intCodes;
+  }
+
+  inline void ensureSpace(vector<long long> &intCodes, size_t index) {
+    if (index >= intCodes.size()) {
+      intCodes.push_back(0ll);
+    }
+  }
+
+  inline long long getValue(vector<long long> &intCodes, size_t index, int paramMode) {
+    ensureSpace(intCodes, index);
+    switch (paramMode) {
+      default:
+      case POS_MODE:
+        return intCodes[intCodes[index]];
+      case IMM_MODE:
+        return intCodes[index];
+      case REL_MODE:
+        return intCodes[intCodes[index] + relativeBase];
+    }
+  }
+  
+  inline void setValue(vector<long long> &intCodes, size_t index, int paramMode, long long newVal) {
+    ensureSpace(intCodes, index);
+    if (paramMode == POS_MODE) {
+      intCodes[intCodes[index]] = newVal;
+    } else if (paramMode == REL_MODE) {
+      intCodes[intCodes[index] + relativeBase] = newVal;
+    } else {
+      intCodes[index] = newVal; // Not possible -- yet;
+    }
+  }
+
+  void processIntCodes(vector<long long> &intCodes, deque<long long> &outputs) {
+    long long intCode, aux1, aux2; 
+    for (int pc = 0;;) {
+      intCode = intCodes[pc];
+      int param[3] = {0, 0, 0};
+      param[0] = (intCode / 100) % 10;
+      param[1] = (intCode / 1000) % 10;
+      param[2] = (intCode / 10000) % 10;
+      
+      switch (intCode %= 100) {
+        case 1:  // fall through
+        case 2:  // Operations
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          aux2 = getValue(intCodes, pc + 2, param[1]);
+          setValue(intCodes, pc + 3, param[2], intCode == 1 ? (aux1 + aux2) : (aux1 * aux2));
+          pc += 4;
+          break;
+        case 3: // Input
+          setValue(intCodes, pc + 1, param[0], outputs.front());
+          outputs.pop_front();
+          pc += 2;
+          break;
+        case 4:  // Output
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          outputs.push_back(aux1);
+          cout << aux1 << endl;
+          pc += 2;
+          break;
+        case 5:  // Non-Zero Jump
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          aux2 = getValue(intCodes, pc + 2, param[1]);
+          pc = aux1 ? aux2 : pc + 3;
+          break;
+        case 6:  // Eq-Zero Jump 
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          aux2 = getValue(intCodes, pc + 2, param[1]);
+          pc = (aux1 == 0 ? aux2 : pc + 3);
+          break;
+        case 7:  // Less-than Jump
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          aux2 = getValue(intCodes, pc + 2, param[1]);
+          setValue(intCodes, pc + 3, param[2], (aux1 < aux2 ? 1 : 0));
+          pc += 4;
+          break;
+        case 8:  // Equals Jump
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          aux2 = getValue(intCodes, pc + 2, param[1]);
+          setValue(intCodes, pc + 3, param[2], (aux1 == aux2 ? 1 : 0));
+          pc += 4;
+          break;
+        case 9:  // Update relative base
+          aux1 = getValue(intCodes, pc + 1, param[0]);
+          relativeBase += aux1;
+          pc += 2;
+          break;
+        case 99:
+        default:
+          return;
+      }
+    }
   }
 
   void solve(int part = 1) {
     using namespace std;
+    string input;
+    cin >> input;
+    vector<long long> intCodes = getIntCodes(input);
+    deque<long long> outputs;
+    outputs.push_back(1ll);  
+    processIntCodes(intCodes, outputs);
     if (part == 1) {
-      solve1();
+      // initialPhaseSettings = {0,0,0,0,0};
     } else {
-      solve2();
+      // initialPhaseSettings = {5,6,7,8,9};
     }
   }
 };
