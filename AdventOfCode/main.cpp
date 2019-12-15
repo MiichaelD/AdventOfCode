8a50b35ea8123af3de270988303c4d26bfb7fcb3
Michael Duarte <michael.duarte@hotmail.com>
2019-12-14 17:40:37 -0800

    Adds all the 2019 includes in main.cpp

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,7 +1,20 @@
+
+#include "2019/AdventOfCode_01.h"
+#include "2019/AdventOfCode_02.h"
+#include "2019/AdventOfCode_03.h"
+#include "2019/AdventOfCode_04.h"
+#include "2019/AdventOfCode_05.h"
+#include "2019/AdventOfCode_06.h"
+#include "2019/AdventOfCode_07.h"
+#include "2019/AdventOfCode_08.h"
+#include "2019/AdventOfCode_09.h"
+#include "2019/AdventOfCode_10.h"
+#include "2019/AdventOfCode_11.h"
 #include "2019/AdventOfCode_12.h"
+#include "2019/AdventOfCode_13.h"
 
 // Compiling:    g++ -std=c++11 main.cpp -o main
 int main() {
-  aoc2019_12::solve();
+  aoc2019_13::solve();
   return EXIT_SUCCESS;
 }
