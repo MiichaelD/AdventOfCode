a5335469cf7fc27b7a9190a6836c4e9bff04341c
Michael Duarte <michael.duarte@hotmail.com>
2019-12-19 22:05:52 -0800

    Adds code for finding the portal in the map

--- AdventOfCode/2019/AdventOfCode_20.h
+++ AdventOfCode/2019/AdventOfCode_20.h
@@ -1,12 +1,12 @@
 /*
-  Link:         http://adventofcode.com/2019/day/X
+  Link:         http://adventofcode.com/2019/day/20
   Compiling:    g++ -std=c++11 main.cpp -o main
   Programmer:   Michael Duarte.
-  Date:         12/XX/2019
+  Date:         12/20/2019
 */
 
-#ifndef _2019_ADVENTOFCODE_XX_H_
-#define _2019_ADVENTOFCODE_XX_H_
+#ifndef _2019_ADVENTOFCODE_20_H_
+#define _2019_ADVENTOFCODE_20_H_
 
 #include <algorithm> // std::sort
 #include <iomanip>      // std::setprecision
@@ -22,26 +22,70 @@
 #include <unordered_set>
 #include <vector>
 
-namespace aoc2019_XX {  
+namespace aoc2019_20 {  
   using namespace std;
 
-  void solve1() {   
-    string input;
-    cin >> input;
-    
+  typedef unordered_map<string, vector<pair<int,int>>> PORTAL_MAP;
+  typedef vector<string> MAP;
+
+  const char WALL1 = '#';
+  const char WALL2 = ' ';
+
+  template<class T>
+  inline void printPair(const pair<T,T> &p) {
+    cout << "[" << p.first << ", " << p.second << "]";
   }
 
-  void solve2() {    
-    
+  void printPortals(const PORTAL_MAP& portals) {
+    for (const auto &entry : portals) {
+      cout << "Key: " << entry.first << " has following positions: ";
+      for (const auto &p : entry.second) {
+        printPair(p); cout << ", ";
+      }
+      cout << endl;
+    }
+  }
+
+  void printMap(const MAP &map) {
+    for (const string &line : map) {
+      for (char c : line) {
+        // if (c == WALL2) c = WALL1;
+        cout << c;
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+
+  void findPortals(const MAP &map, PORTAL_MAP &portals) {
+    for (int y = 0; y < map.size(); ++y) {
+      for (int x = 0; x < map[y].size(); ++x) {
+        char c = map[y][x];
+        // Skip non alpha chars, and end-of-strings (bottom or right chars)
+        if (!isalpha(c) || isalpha(map[y][x-1]) || isalpha(map[y-1][x])) continue;
+        if (isalpha(map[y][x+1])) {
+          string key(2,c);  key[1] = map[y][x+1];
+          portals[key].push_back(make_pair(x+1, y)); 
+        } else if (isalpha(map[y+1][x])) {
+          string key(2,c);  key[1] =  map[y+1][x];
+          portals[key].push_back(make_pair(x, y+1)); 
+        }
+      }
+    }
   }
 
   void solve(int part = 1) {
-    if (part == 1) {
-      solve1();
-    } else {
-      solve2();
+    string input;
+    MAP map;
+    PORTAL_MAP portals;
+    while (!cin.eof()) {
+      getline(cin, input);
+      map.push_back(input);
     }
+    printMap(map);
+    findPortals(map, portals);
+    printPortals(portals);
   }
 };
 
-#endif /* _2019_ADVENTOFCODE_XX_H_ */
+#endif /* _2019_ADVENTOFCODE_20_H_ */
--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -18,9 +18,10 @@
 #include "2019/AdventOfCode_17.h"
 #include "2019/AdventOfCode_18.h"
 #include "2019/AdventOfCode_19.h"
+#include "2019/AdventOfCode_20.h"
 
 // Compiling:    g++ -std=c++11 main.cpp -o main
 int main() {
-  aoc2019_12::solve(2);
+  aoc2019_20::solve();
   return EXIT_SUCCESS;
 }
