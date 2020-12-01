31d7d15c7674df3a73454421f558b25e4c2ab51f
Michael Duarte <michael.duarte@hotmail.com>
2020-11-30 22:24:44 -0800

    Initial commit 2020

---
+++ AdventOfCode/2020/AdventOfCode_03.h
@@ -0,0 +1,36 @@
+/*
+  Link:         http://adventofcode.com/2020/day/3
+ 
+  Description: 
+  Compiling:    g++ -std=c++11 main.cpp -o main
+               
+  Programmer:   Michael Duarte.
+
+  Date:         12/03/2020
+*/
+
+#ifndef _2020_ADVENTOFCODE_03_H_
+#define _2020_ADVENTOFCODE_03_H_
+
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+namespace aoc2020_03 {
+  using namespace std;
+
+  unsigned long long solve1() {
+  }
+
+  unsigned long long solve2() {
+  }
+
+  void solve(int part = 1) {
+    using namespace std;
+    cout << (part == 1 ? solve1() : solve2()) << endl;
+  }
+
+};
+
+#endif /* _2020_ADVENTOFCODE_03_H_ */
--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,29 +1,29 @@
 
-#include "2019/AdventOfCode_01.h"
-#include "2019/AdventOfCode_02.h"
-#include "2019/AdventOfCode_03.h"
-#include "2019/AdventOfCode_04.h"
-#include "2019/AdventOfCode_05.h"
-#include "2019/AdventOfCode_06.h"
-#include "2019/AdventOfCode_07.h"
-#include "2019/AdventOfCode_08.h"
-#include "2019/AdventOfCode_09.h"
-#include "2019/AdventOfCode_10.h"
-#include "2019/AdventOfCode_11.h"
-#include "2019/AdventOfCode_12.h"
-#include "2019/AdventOfCode_13.h"
-#include "2019/AdventOfCode_14.h"
-#include "2019/AdventOfCode_15.h"
-#include "2019/AdventOfCode_16.h"
-#include "2019/AdventOfCode_17.h"
-#include "2019/AdventOfCode_18.h"
-#include "2019/AdventOfCode_19.h"
-#include "2019/AdventOfCode_20.h"
-#include "2019/AdventOfCode_21.h"
-#include "2019/AdventOfCode_22.h"
-#include "2019/AdventOfCode_23.h"
-#include "2019/AdventOfCode_24.h"
-#include "2019/AdventOfCode_25.h"
+#include "2020/AdventOfCode_01.h"
+#include "2020/AdventOfCode_02.h"
+#include "2020/AdventOfCode_03.h"
+#include "2020/AdventOfCode_04.h"
+#include "2020/AdventOfCode_05.h"
+// #include "2020/AdventOfCode_06.h"
+// #include "2020/AdventOfCode_07.h"
+// #include "2020/AdventOfCode_08.h"
+// #include "2020/AdventOfCode_09.h"
+// #include "2020/AdventOfCode_10.h"
+// #include "2020/AdventOfCode_11.h"
+// #include "2020/AdventOfCode_12.h"
+// #include "2020/AdventOfCode_13.h"
+// #include "2020/AdventOfCode_14.h"
+// #include "2020/AdventOfCode_15.h"
+// #include "2020/AdventOfCode_16.h"
+// #include "2020/AdventOfCode_17.h"
+// #include "2020/AdventOfCode_18.h"
+// #include "2020/AdventOfCode_19.h"
+// #include "2020/AdventOfCode_20.h"
+// #include "2020/AdventOfCode_21.h"
+// #include "2020/AdventOfCode_22.h"
+// #include "2020/AdventOfCode_23.h"
+// #include "2020/AdventOfCode_24.h"
+// #include "2020/AdventOfCode_25.h"
 
 // Compiling:    g++ -std=c++11 main.cpp -o main; ./main [<part>]
 int main(int argc, char *argv[]) {
@@ -32,6 +32,6 @@ int main(int argc, char *argv[]) {
     part = atoi(argv[argc - 1]);
   }
   std::cout << "Solving part: " << part << std::endl;
-  aoc2019_25::solve(part);
+  aoc2020_01::solve(part);
   return EXIT_SUCCESS;
 }
