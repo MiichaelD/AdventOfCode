30530706b56bcdd94b3f70f0a1bb8c1c377a7493
Michael Duarte <michael.duarte@hotmail.com>
2020-12-01 21:51:21 -0800

    Updates 2020/02 to compute both solutions at the same time

--- AdventOfCode/2020/AdventOfCode_02.h
+++ AdventOfCode/2020/AdventOfCode_02.h
@@ -16,6 +16,7 @@
 #include <string>
 #include <unordered_set>
 #include <vector>
+#include "../util/util.h"
 
 namespace aoc2020_02 {
   using namespace std;
@@ -25,11 +26,6 @@ namespace aoc2020_02 {
     char letter;
     Policy (pair<int,int> &&range, char letter) : range(range), letter(letter) {}
   };
-
-  template<class T>
-  inline void print(const pair<T,T> &p) {
-    cout << "[" << p.first << ", " << p.second << "] ";
-  }
 
   pair<int,int> parseRange(const string &input) {
     int i = 0, j = 0;
@@ -50,54 +46,46 @@ namespace aoc2020_02 {
 
   bool checkOldPasswordValidity(const Policy &p, const string &pass) {
     int reps = 0;
-    cout << "Checking '" << pass << "' - letter " << p.letter << ". ";
-    print(p.range); cout << ". Found: ";
+    // cout << "Checking '" << pass << "' - letter " << p.letter << ". ";
+    // print(p.range); cout << ". Found: ";
     for (char c : pass) {
       if (c == p.letter) ++reps;
     }
     bool result = reps >= p.range.first && reps <= p.range.second;
-    cout << reps << "\t" << (result ? "valid" : "invalid") << endl;
+    // cout << reps << "\t" << (result ? "valid" : "invalid") << endl;
     return result;
   }
 
   bool checkPasswordValidity(const Policy &p, const string &pass) {
     int reps = 0;
-    cout << "Checking '" << pass << "' - letter " << p.letter << ". ";
-    print(p.range); cout << ". Found: ";
+    // cout << "Checking '" << pass << "' - letter " << p.letter << ". ";
+    // print(p.range); cout << ". Found: ";
     if (pass[p.range.first - 1] == p.letter) ++reps;
     if (pass[p.range.second - 1] == p.letter) ++reps;
     bool result = reps == 1;
-    cout << reps << "\t" << (result ? "valid" : "invalid") << endl;
+    // cout << reps << "\t" << (result ? "valid" : "invalid") << endl;
     return result;
   }
 
-  unsigned long long solve1() {
+  pair<int,int> solveBoth() {
     string rangeInput, letter, password;
-    unsigned long long validPasswords = 0;
+    int oldValid = 0, newValid = 0;
     while(!std::cin.eof()) {
-      cin >> rangeInput >> letter >> password;
+      cin >> rangeInput;
+      if (std::cin.eof()) break; // In case there is an extra line
+      cin >> letter >> password;
       Policy policy(parseRange(rangeInput), letter[0]);
       if (checkOldPasswordValidity(policy, password))
-        ++validPasswords;
-    }
-    return validPasswords;
-  }
-
-  unsigned long long solve2() {
-    string rangeInput, letter, password;
-    unsigned long long validPasswords = 0;
-    while(!std::cin.eof()) {
-      cin >> rangeInput >> letter >> password;
-      Policy policy(parseRange(rangeInput), letter[0]);
+        ++oldValid;
       if (checkPasswordValidity(policy, password))
-        ++validPasswords;
+        ++newValid;
     }
-    return validPasswords;
+    return std::make_pair(oldValid, newValid);
   }
 
   void solve(int part = 1) {
     using namespace std;
-    cout << (part == 1 ? solve1() : solve2()) << endl;
+    util::printPair(solveBoth(), true);
   }
 
 };
