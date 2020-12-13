7bb55a5b0ee5d6c5f39ede2d3b399b1d8647b88c
Michael Duarte <michael.duarte@hotmail.com>
2020-12-13 14:11:33 -0800

    Adds 2020/13 - Got to look for math videos/articles and examples

--- AdventOfCode/2020/AdventOfCode_13.h
+++ AdventOfCode/2020/AdventOfCode_13.h
@@ -28,7 +28,7 @@
 namespace aoc2020_13 {
 using namespace std;
 
-typedef long long int lli;
+typedef uint64_t lli;
 typedef pair<lli,size_t> Bus;
 
 lli gcd(lli a, lli b) {
@@ -127,15 +127,41 @@ lli getTimeT(const vector<Bus> &busses) {
   return 0ll;
 }
 
+// https://github.com/Chrinkus/advent-of-code-2020/blob/main/src/day13.cpp
+// Manually find a stride that is common for current bus and adjacent bus index.
+lli solveChallenge(const vector<Bus> &boffs) {
+  auto it = boffs.begin();
+  lli mins = it->first, stride = mins;
+  cout << "Bus [" << it->second << "]: Every :" << it->first << " mins" << endl;
+  cout << "\tMins: " <<  mins << ", Stride: " << stride << endl;
+  for (it = next(it); it != boffs.end(); ++it) {
+    cout << "Bus [" << it->second << "]: " << it->first << endl;
+    size_t times = 0;
+    while ((mins + it->second) % it->first != 0) {
+      cout << "\t\t" << (mins + it->second) << " % " << it->first << " = ";
+      cout << (mins + it->second) % it->first << endl;
+      mins += stride;
+      ++times;
+    }
+    stride *= it->first;
+    cout << "\t\t" << (mins + it->second) << " % " << it->first << " = ";
+    cout << (mins + it->second) % it->first << endl;
+    cout << "\tMins: " <<  mins << ", Stride: " << stride << ", Times: " << times << endl;
+  }
+  return mins;
+}
+
 void solve(int part = 1) {
   lli earliest, result = 0ll;
   cin >> earliest;  
   vector<Bus> busses;
   fillBusses(&busses);
+  // sort(busses.begin(), busses.end(),
+  //      [](const Bus &a, const Bus &b) {return a.first < b.first;});
   if (part == 1) {
     result = getEarliestShuttlePickup(busses, earliest);
   } else {
-    result = getTimeT(busses);
+    result = solveChallenge(busses);
   }
   cout << "Result: " << result << endl;
 }
@@ -143,3 +169,43 @@ void solve(int part = 1) {
 };  // aoc2020_13
 
 #endif /* _2020_ADVENTOFCODE_13_H_ */
+
+/*
+
+tuple<lli,lli,lli> egcd(lli a, lli b) {
+  if (a == 0) {
+    return {b, 0ll, 1ll};
+  } else {
+    tuple<lli,lli,lli> res = egcd(b % a, a);
+    // const auto [g, x, y] = egcd(b % a, a);  // C++-17
+    lli g = get<0>(res), x = get<1>(res), y = get<2>(res);
+    return {g, y - (b / a) * x, x};
+  }
+}
+
+lli mod_inv(lli a, lli n) {
+  // x = gcd(a, n);
+  lli x = get<1>(egcd(a, n));
+  // const auto [g, x, y] = egcd(a, n);  // C++-17
+  // x = lcm(x, n);
+  return (x % n + n) % n;
+}
+
+// https://en.wikipedia.org/wiki/Chinese_remainder_theorem#Search_by_sieving
+// https://www.youtube.com/watch?v=0dbXaSkZ-vc
+lli chineseRemainderTheorem(const vector<Bus> &busses) {
+  lli product = 1;
+  for (size_t j = 0; j < busses.size(); ++j) {
+    product *= busses[j].first;
+  }
+  lli accum = 0;
+
+  for (size_t j = 0; j < busses.size(); ++j) {
+    const Bus &bus = busses[j];
+    lli p = product / bus.first;
+    accum = accum +  bus.second  + mod_inv(p, bus.first) * p;
+  }
+  return accum % product;;
+}
+
+*/
