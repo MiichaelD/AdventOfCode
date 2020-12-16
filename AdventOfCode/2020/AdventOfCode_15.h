ba5f1d0009ad13841c9556d34f149be2b0248c1a
Michael Duarte <michael.duarte@hotmail.com>
2020-12-15 18:18:55 -0800

    Adds 2020/15 - vectors and maps

--- AdventOfCode/2020/AdventOfCode_15.h
+++ AdventOfCode/2020/AdventOfCode_15.h
@@ -28,9 +28,35 @@
 namespace aoc2020_15 {
 using namespace std;
 
-void solve1() {
-  string input;
-  cin >> input;  
+vector<uint64_t> getInput() {
+  vector<uint64_t> input;
+  string line;
+  cin >> line;
+  uint64_t accum = 0;
+  size_t bInd = 0;
+  for (size_t index = 0; index < line.size(); ++index) {
+    if (isdigit(line[index])) {
+      accum *= 10;
+      accum += line[index] - '0';
+    } else if (line[index] == ',') {
+      input.push_back(accum);
+      accum = 0;
+    }
+  }
+  if (accum != 0) {
+    input.push_back(accum);
+    accum = 0;
+  }
+  return input;
+}
+
+void speakNumber(
+  uint64_t number, unordered_map<uint64_t,vector<size_t>> &seen, vector<uint64_t> &input) {
+    seen[number].push_back(input.size());
+    input.push_back(number);
+}
+
+void solve1(vector<uint64_t> &input) {
 }
 
 void solve2() {
@@ -39,11 +65,27 @@ void solve2() {
 }
 
 void solve(int part = 1) {
-  if (part == 1) {
-    solve1();
-  } else {
-    solve2();
+  size_t kTotalTurns = part == 1 ? 2020 : 30000000;
+  vector<uint64_t> input = getInput();
+  unordered_map<uint64_t,vector<size_t>> seen;
+  uint64_t last;
+  for (size_t index = 0; index < input.size(); ++index) {
+    last = input[index];
+    cout << "Turn " << (index + 1) << ": " << last << endl;
+    seen[last].push_back(index);
   }
+  while (input.size() < kTotalTurns) {
+    const auto &it = seen.find(last);
+    vector<size_t> &reps = it->second;
+    // for (int i = 0 ; i < reps.size(); ++i ){
+    //   cout << "\tNumber " << last << " has been seen @ " << (reps[i] + 1) << endl;
+    // }
+    uint64_t number = it->second.size() < 2 ? 0 : (reps.back() - reps.at(reps.size() - 2));
+    cout << "Turn " << (input.size() + 1) << ": " << number << endl;
+    speakNumber(number, seen, input);
+    last = number;
+  }
+  cout << "Turn " << input.size() << ": " << last << endl;
 }
 
 };  // aoc2020_15
