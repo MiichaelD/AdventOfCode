a8616a6d5788edc9db64edbe79d7779b0aad74e4
Michael Duarte <michael.duarte@hotmail.com>
2020-12-15 21:52:04 -0800

    Adds 2020/16 pt 1 - parsing was time consuming

--- AdventOfCode/2020/AdventOfCode_16.h
+++ AdventOfCode/2020/AdventOfCode_16.h
@@ -28,9 +28,151 @@
 namespace aoc2020_16 {
 using namespace std;
 
+struct Range {
+  string name;
+  vector<pair<int,int>> rules;
+
+  bool isInRange(int val) const {
+    for (const auto &p : rules) {
+      if (p.first <= val && val <= p.second) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void print() const {
+    cout << "\tRange '" << name << "':" << endl;
+    for (const auto &r : rules) {
+      cout << "\t\t"; util::printPair(r, true);
+    }
+  }
+};
+
+struct Ticket {
+  vector<size_t> values;
+  bool discarded = false;
+}
+
+struct Data{
+  vector<Range> ranges;
+  vector<Ticket> tickets;
+
+  void print() const {
+    cout << "Ranges: " << ranges.size() << endl;
+    for (const auto &r : ranges) {
+      r.print();
+    }
+    cout << "Tickets: " << tickets.size() << endl;
+    // for (const auto &ticket : tickets) {
+    //   cout << "\t";
+    //   for (size_t t : ticket) {
+    //     cout << ", " << t;
+    //   }
+    //   cout << endl;
+    // }
+  }
+};
+
+pair<int,int> getRangeRule(const string &line, size_t &index) {
+  pair<int,int> result{-1, -1};
+  int accum = 0;
+  for (; index < line.size(); ++index) {
+    if (isdigit(line[index])) {
+      accum *= 10;
+      accum += line[index] - '0';
+    } else if (line[index] == '-') {
+      result.first = accum;
+      accum = 0;
+    } else if (line[index] == ' ') {
+      result.second = accum;
+      break;
+    }
+  }
+  if (result.second == -1) {
+    result.second = accum;
+  }
+  return result;
+}
+
+vector<size_t> getTicketData() {
+  vector<size_t> input;
+  string line;
+  cin >> line;
+  uint64_t accum = 0;
+  size_t bInd = 0;
+  for (size_t index = 0; index < line.size(); ++index) {
+    if (isdigit(line[index])) {
+      accum *= 10;
+      accum += line[index] - '0';
+    } else if (line[index] == ',') {
+      input.push_back(accum);
+      accum = 0;
+    }
+  }
+  if (accum != 0) {
+    input.push_back(accum);
+    accum = 0;
+  }
+  return input;
+}
+
+Data getInput() {
+  Data result;
+  string str, line;
+  vector<string> lines;
+  stringstream ss;
+  do {
+    getline(cin,line);
+    if (line.empty()) break;
+    result.ranges.push_back({});
+    size_t index = 0;
+    // Getting Range name
+    for (; index < line.size(); ++index) {
+      if (line[index] == ':') {
+        result.ranges.back().name = line.substr(0, index);
+        break;
+      }
+    }
+    index += 2;
+    result.ranges.back().rules.push_back(getRangeRule(line, index));
+    index += 4;
+    result.ranges.back().rules.push_back(getRangeRule(line, index));
+  } while(!line.empty());
+
+  getline(cin,line);
+  result.tickets.emplace_back(getTicketData());
+  getline(cin,line);
+  getline(cin,line);
+  getline(cin,line);
+  while(!cin.eof()) {
+    result.tickets.emplace_back(getTicketData());
+  }
+  return result;
+}
+
 void solve1() {
-  string input;
-  cin >> input;  
+  Data input = getInput();
+  input.print();
+
+  uint64_t result = 0;
+  for (auto &ticket : input.tickets) {
+    for (auto &t : ticket.values) {
+      bool isValid = false;
+      for (const auto &r: input.ranges) {
+        if (r.isInRange(t)) {
+          isValid = true;
+          break;
+        }
+      }
+      if (!isValid) {
+        cout << "Ticket value " << t << " not in any valid range" << endl;
+        ticket.discarded = true;
+        result += t.val;
+      }
+    }
+  }
+  cout << "Ticket staning error rate: " << result << endl;
 }
 
 void solve2() {
