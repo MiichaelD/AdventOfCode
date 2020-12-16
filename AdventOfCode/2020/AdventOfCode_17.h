f5053a66c10965b89e55529d3622d706b2a51d6e
Michael Duarte <michael.duarte@hotmail.com>
2020-12-16 00:02:40 -0800

    Adds 2020/[17-20] template files

---
+++ AdventOfCode/2020/AdventOfCode_17.h
@@ -0,0 +1,51 @@
+/*
+  Link:         http://adventofcode.com/2020/day/17
+  Compiling:    g++ -std=c++11 main.cpp -o main
+  Programmer:   Michael Duarte.
+  Date:         12/17/2020
+*/
+
+#ifndef _2020_ADVENTOFCODE_17_H_
+#define _2020_ADVENTOFCODE_17_H_
+
+#include <algorithm> // std::sort
+#include <iomanip>      // std::setprecision
+#include <iostream> // std::cin, std::cout, std::fixed
+#include <cstring>  // strcmp()
+#include <ctype.h>  // char related functions
+#include <climits>  // INT_MAX and such numeric limits
+#include <cmath>   // pow, floor, sqrt
+#include <deque>
+#include <limits> // std::numeric_limits<T>::max
+#include <memory>
+#include <sstream>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include "../util/util.h"
+
+namespace aoc2020_17 {
+using namespace std;
+
+void solve1() {
+  string input;
+  cin >> input;  
+}
+
+void solve2() {
+  string input;
+  cin >> input;
+}
+
+void solve(int part = 1) {
+  if (part == 1) {
+    solve1();
+  } else {
+    solve2();
+  }
+}
+
+};  // aoc2020_17
+
+#endif /* _2020_ADVENTOFCODE_17_H_ */
