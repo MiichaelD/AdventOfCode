e949351af14159a030f84e5eb220dfe4b120957a
Michael Duarte <michael.duarte@hotmail.com>
2021-11-30 23:56:14 -0800

    Adds comments to 2021/01

--- AdventOfCode/2021/AdventOfCode_01.h
+++ AdventOfCode/2021/AdventOfCode_01.h
@@ -1,25 +1,24 @@
 /*
-  Link:         http://adventofcode.com/2020/day/1
+  Link:         http://adventofcode.com/2021/day/1
  
-  Description: 
-  Compiling:    g++ -std=c++11 main.cpp -o main
+  Compiling:     g++ -std=c++17 -o main main.cpp 
+  Running:       cat 2021/AdventOfCode_01_input.txt | ./main
                
   Programmer:   Michael Duarte.
 
-  Date:         12/01/2020
+  Date:         11/30/2021
 */
 
 #ifndef _2021_ADVENTOFCODE_01_H_
 #define _2021_ADVENTOFCODE_01_H_
 
 #include <iostream>
-#include <string>
-#include <unordered_set>
-#include <vector>
+#include <deque>
 
 namespace aoc2021_01 {
   using namespace std;
 
+  // Iterate each line, count the amount of times that current value is bigger than the last.
   unsigned long long solve1() {
     int prevEntry, entry, delta = 0;
     cin >> prevEntry;
@@ -33,15 +32,16 @@ namespace aoc2021_01 {
     return delta;
   }
 
+  // Keep a sliding window of 3 items, count the amount of times that the current sliding window's
+  // sum is greater than the previous sliding window's sum.
   unsigned long long solve2() {
     deque<int> slidingWindow;
     int accum = 0, entry = 0, delta = 0, prevAccum = -1;
     while(!cin.eof()) {
       cin >> entry;
-      slidingWindow.push_back(entry);
       accum += entry;
+      slidingWindow.push_back(entry);
       if (slidingWindow.size() == 3) {
-        cout << "\tAccum: " << accum << endl;
         if (prevAccum != -1 && prevAccum < accum) {
           ++delta;
         }
@@ -56,7 +56,5 @@ namespace aoc2021_01 {
   void solve(int part = 1) {
     cout << (part == 1 ? solve1() : solve2()) << endl;
   }
-
 };
-
 #endif /* _2021_ADVENTOFCODE_01_H_ */
