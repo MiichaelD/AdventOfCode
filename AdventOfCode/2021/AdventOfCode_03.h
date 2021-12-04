e06d11a67fe9f802cc6c39b987aab56b6ed9472d
Michael Duarte <michael.duarte@hotmail.com>
2021-12-03 19:29:26 -0800

    Adds 2021/03 pt 2: bit comparissons

--- AdventOfCode/2021/AdventOfCode_03.h
+++ AdventOfCode/2021/AdventOfCode_03.h
@@ -22,13 +22,70 @@
 namespace aoc2021_03 {
   using namespace std;
 
-  pair<string,string> getGammaAndEpsilon(const vector<int> &reps, size_t total) {
+  inline int getPopularBit(int reps, int common) {
+    cout << "\t\tReps: " << reps << " - Common: "<< common << endl;
+    if (reps == common) {
+      return -1; // no popular bit
+    }
+    return (reps > common ? 1 : 0);
+  }
+
+  int getRepetition(const vector<string> &entries,  vector<int> &indexOfInterest, int bitOfInterest) {
+    int repetition = 0;
+    for (int index : indexOfInterest) {
+      repetition += entries[index][bitOfInterest] == '1' ? 1 : 0;
+    }
+    return repetition;
+  }
+
+
+  pair<string,string> getOxygenAndCO2Rating(
+    const vector<string> &entries, vector<int> &repetitions) {
+    int common = entries.size() / 2;
+    const int totalRatings = 2;
+    vector<int> ratingIndex(totalRatings);
+    for (int rating = 0; rating < totalRatings; ++rating) {
+      int repetition = repetitions.front();
+      vector<int> indexOfInterest(entries.size());
+      for (int i = 1; i < entries.size(); ++i) {
+        indexOfInterest[i] = i;
+      }
+      int bitOfInterest = 0;
+      while(indexOfInterest.size() > 1) {
+        common = indexOfInterest.size();
+        int popularBit = getPopularBit(repetition * 2, common);
+        if (rating == 1 && popularBit != -1) {
+          popularBit = popularBit == 1 ? 0 : 1;  // flip it
+        }
+        cout << (bitOfInterest + 1) << " popular bit: " << popularBit;
+        cout << "\t Entries remaining: " << indexOfInterest.size() << endl;
+        vector<int> indexOfInterestTmp;
+        for (int index : indexOfInterest) {
+          cout << "Checking index: " << index << ": " << entries[index] << endl;
+          int entryBit = entries[index][bitOfInterest] - '0';
+          if ((entryBit == popularBit)
+              || (popularBit == -1  && ((rating == 0 && entryBit == 1) || (rating == 1 && entryBit == 0)))) {
+            cout << "\tInterest: " << entries[index] << endl << endl;
+            indexOfInterestTmp.push_back(index);
+          }
+        }
+        cout << endl;
+        repetition = getRepetition(entries, indexOfInterestTmp, ++bitOfInterest);
+        indexOfInterest = indexOfInterestTmp;
+      }
+      cout << "Rating " << rating << " -> " << entries[indexOfInterest.front()] << endl << endl;
+      ratingIndex[rating] = indexOfInterest.front();
+    } 
+    return {entries[ratingIndex.front()], entries[ratingIndex.back()]};
+  }
+
+  pair<string,string> getGammaAndEpsilon(const vector<int> &reps, int total) {
     stringstream gamma, epsilon;
-    size_t common = total / 2;
-    for (const int element : reps) {
-      bool popularBit = element >= common;
-      gamma << (popularBit ? '1' : '0');
-      epsilon << (popularBit ? '0' : '1');
+    int common = total / 2;
+    for (const int elementReps : reps) {
+      int popularBit = getPopularBit(elementReps, common);
+      gamma << popularBit;
+      epsilon << (popularBit == 1 ? '0' : '1');
     }
     return {gamma.str(), epsilon.str()};
   }
@@ -60,15 +117,17 @@ namespace aoc2021_03 {
     return gamma * epsilon;
   }
 
-  size_t solve2() {string input;
+  uint64_t solve2() {
+    string input;
+    vector<string> entries;
     vector<int> repetitions;
-    int totalEntries = 0;
+    // Process input.
     while(!cin.eof()) {
       cin >> input;
       if (input.empty()) {
         break;
       }
-      ++totalEntries;
+      entries.push_back(input);
       if (repetitions.size() < input.size()) {
         repetitions.resize(input.size());
       }
@@ -78,12 +137,12 @@ namespace aoc2021_03 {
       }
     }
     util::printVector(repetitions);
-    auto gammaAndEpsilon = getGammaAndEpsilon(repetitions, totalEntries);
-    util::printPair(gammaAndEpsilon, true);
-    uint64_t gamma = util::binaryToDecimal(gammaAndEpsilon.first);
-    uint64_t epsilon = util::binaryToDecimal(gammaAndEpsilon.second);
-    cout << "Gamma: " << gamma << ". Epsilon: " << epsilon << endl;
-    return gamma * epsilon;
+    auto ratings = getOxygenAndCO2Rating(entries, repetitions);
+    util::printPair(ratings, true);
+    uint64_t oxygen = util::binaryToDecimal(ratings.first);
+    uint64_t co2 = util::binaryToDecimal(ratings.second);
+    cout << "Oxygen Rating: " << oxygen << ". CO2 Rating: " << co2 << endl;
+    return oxygen * co2;
   }
 
   void solve(int part = 1) {
