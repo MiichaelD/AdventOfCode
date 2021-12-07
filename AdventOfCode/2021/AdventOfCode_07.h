639a3c037b73174739786ec3ff4c17ac03f1a167
Michael Duarte <michael.duarte@hotmail.com>
2021-12-06 21:52:02 -0800

    2021/07: Array: min, max, accum, gauss formula

--- AdventOfCode/2021/AdventOfCode_07.h
+++ AdventOfCode/2021/AdventOfCode_07.h
@@ -27,109 +27,53 @@
 namespace aoc2021_07 {
 using namespace std;
 
-constexpr char kMyBag[] = "shiny_gold";
-
-typedef unordered_map<string,unordered_map<string,int>> BagRules;
-
-void printBagRules(const BagRules& bagRules) {
-  cout << "Printing bag rules: " << bagRules.size() << endl;
-  for (const auto &rule : bagRules) {
-    cout << "Contents: " << rule.first << ":" << endl; 
-    for (const auto &contents : rule.second) {
-      cout << "\t" << contents.first << ": " << contents.second << endl;
-    }
-  }
-}
-
-inline string composeId(const string &adj, const string &color) {
-  return adj + "_" + color; 
-}
-
-inline bool getQuantityAndMaybeSkip(string &maybeQuantity) {
-  string skip;
-  cin >> maybeQuantity;
-  if (!isdigit(maybeQuantity[0])) {
-    cin >> skip >> skip;  // consume 2 spaces "other bags."
-    return false;
-  }
-  return true;
-}
-
-BagRules getInput() {
-  string adjective, color, id, skip, bag, maybeQuantity;
-  int quantity;
-  vector<string> lines;
-  stringstream idStream;
-  BagRules bagRules;
-  while(!cin.eof()) {
-    cin >> adjective >> color >> bag >> skip;
-    id = composeId(adjective, color);
-    if (!getQuantityAndMaybeSkip(maybeQuantity)) {
-      bagRules[id];  // initialize bag with no more data.
-      continue;
-    }
-    cin >> adjective >> color >> bag;
-    bagRules[id][composeId(adjective, color)] = atoi(maybeQuantity.c_str());
-    while (bag[bag.size()-1] != '.') {
-      if (!getQuantityAndMaybeSkip(maybeQuantity)) {
-        continue;
-      }
-      cin >> adjective >> color >> bag;
-      bagRules[id][composeId(adjective, color)] = atoi(maybeQuantity.c_str());
-    }
-  }
-  return bagRules;
-}
-
-size_t findBagsContainingMyBag(const BagRules &bagRules) {
-  unordered_set<string> checked;
-  deque<string> toCheck({kMyBag});
-  while (!toCheck.empty()) {
-    // Find all bags that may contain this bag;
-    for (const auto &rule : bagRules) {
-      if (rule.second.find(toCheck.front()) != rule.second.end()) {
-        toCheck.push_back(rule.first);
-      }
-    }
-    checked.insert(toCheck.front());
-    toCheck.pop_front();
-  }
-  return checked.size() - 1;
-}
-
-size_t findTotalBagsContainedInMyBag(
-    const BagRules &bagRules,
-    unordered_map<string,size_t> &seenBags,
-    const string &checkingBag) {
-  // cout << "Checking: " << checkingBag << endl;
-  size_t result = 0;
-  unordered_set<string> checked;
-  deque<string> toCheck({kMyBag});
-  for (const auto &entry : bagRules.at(checkingBag)) {
-    // cout << "\t" << entry.first  << " * " << entry.second << endl;
-    if (seenBags.find(entry.first) != seenBags.end()) {
-      result += entry.second + (entry.second * seenBags[entry.first]);
-    } else {
-      result += entry.second +
-          (entry.second * findTotalBagsContainedInMyBag(
-            bagRules, seenBags, entry.first));
-    }
-  }
-  seenBags[checkingBag] = result;
-  return result;
-}
 
 void solve(int part = 1) {
-  BagRules bagRules = getInput();
-  if (part == 1) {
-    cout << "Total bags that may contain mine: "
-        << findBagsContainingMyBag(bagRules) << endl;
-  } else {
-    unordered_map<string,size_t> seenBags;
-    cout << "My bag has: "
-        << findTotalBagsContainedInMyBag(bagRules, seenBags, kMyBag)
-        << " Other bags" << endl;
+  string input;
+  cin >> input;
+  vector<int> positions;
+  int sum = 0;
+  int min = INT_MAX, max = INT_MIN;
+  for (int index = 0; index < input.length(); ++index) {
+    int position = util::getNumberRef(input, index);
+    positions.push_back(position);
+    sum += position;
+    if (position < min) min = position;
+    if (position > max) max = position;
   }
+  // cout << "Initial state:\t";
+  // sort(positions.begin(), positions.end());
+  // util::printVector(positions);
+  // cout << "Sum: " << sum << ". Total items: " << positions.size() << endl;
+
+ size_t minIndex1 = 0, minIndex2 = 0;
+//  vector<uint64_t> memo(1 + max - min);  // I thought we were gonna need memoization.
+  int min1 = INT_MAX, min2 = INT_MAX;
+  for (int i = min; i <= max; ++i) {
+    int totalEnergy = 0;
+    int totalEnergy2 = 0;
+    for (int pos : positions) {
+      int energy = abs(pos - i);
+      totalEnergy += energy;
+      int energy2 = (energy * (energy + 1)) / 2;
+      totalEnergy2 += energy2;
+    }
+    // cout << "Energy needed to reach position " << i << ": " << energy;
+    if (totalEnergy < min1) {
+      // cout << "\t <-- This is the position with the current minimum energy needed! ";
+      min1 = totalEnergy;
+      minIndex1 = i;
+    }
+    if (totalEnergy2 < min2) {
+      min2 = totalEnergy2;
+      minIndex2 = i;
+    }
+    // cout << endl;
+  }
+  cout << "For part 1: \t";
+  cout << minIndex1 << " Is the position w/ the minimum energy consumption of: " << min1 << endl;
+  cout << "For part 2: \t";
+  cout << minIndex2 << " Is the position w/ the minimum energy consumption of: " << min2 << endl;
 }
 
 };  // aoc2021_07
