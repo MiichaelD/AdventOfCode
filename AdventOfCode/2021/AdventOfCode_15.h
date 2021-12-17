4627ffd5c6ca7cb5a7a08524e29a51b845aea2d2
Michael Duarte <michael.duarte@hotmail.com>
2021-12-17 12:42:22 -0800

    2021/15 dijkstra: Min cost path finding in grid

--- AdventOfCode/2021/AdventOfCode_15.h
+++ AdventOfCode/2021/AdventOfCode_15.h
@@ -28,6 +28,8 @@
 namespace aoc2021_15 {
 using namespace std;
 
+const size_t dimensionMultiplier = 5;
+
 typedef vector<vector<pair<int,int>>> Matrix; //Matrix<index, sum>
 typedef pair<int,int> Coordinate;
 typedef pair<int,int> Tile;
@@ -35,22 +37,56 @@ typedef pair<int,int> Tile;
 int totalPaths = 0;
 size_t minRiskPathTotal = INT_MAX;
 vector<int> minRiskPath;
+unordered_set<Coordinate, util::pair_hash> minRiskPathCoordinates;
 
-Matrix getInput() {
+inline pair<int,int>& getValue(Matrix &data, const Coordinate &coordinate) {
+  return data[coordinate.second][coordinate.first];
+}
+
+Matrix getInput(int part = 1) {
   string line;
   Matrix result;
+  int lines = 0;
   while(!cin.eof()) {
     cin >> line;
-    result.emplace_back(vector<pair<int,int>>(line.size()));
-    for (int i = 0; i < line.size(); ++i) {
-      result.back()[i].first = line.at(i) - '0'; 
-      result.back()[i].second = INT_MAX; 
+    int matrixDimension = (part == 1 ? line.size() : line.size() * dimensionMultiplier);
+    int dimMultiplier = part == 1 ? 1 : matrixDimension;
+    // result.reserve(matrixDimension);
+    if (part == 1) {
+      result.emplace_back(vector<pair<int,int>>(matrixDimension));
+    } else if (part == 2 && lines == 0) {
+      for (int i = 0 ; i < matrixDimension; ++i) {
+        result.emplace_back(vector<pair<int,int>>(matrixDimension));
+      }
     }
+    cout << "Matrix size: rows: " << result.size() << ", cols: " << result.back().size() << endl; 
+    for (int i = 0; i < line.size(); ++i) {
+      int value = line.at(i) - '0';
+      result[lines][i].first = value;
+      result[lines][i].second = INT_MAX; 
+      if (part == 1) {
+        continue;
+      }
+      for (int tRow = 0; tRow < dimensionMultiplier; ++tRow) {
+        for (int tCol = tRow == 0 ? 1 : 0; tCol < dimensionMultiplier; ++tCol) {
+          int delta = tRow + tCol;
+          int temp = (value + delta);
+          if (temp >= 10) {
+            temp -= 9;
+          }
+          int tileRow = (tRow * line.size());
+          int tileCol = (tCol * line.size());
+          result[lines + tileRow][i + tileCol].first = temp;
+          result[lines + tileRow][i + tileCol].second = INT_MAX;
+        } 
+      }
+    }
+    ++lines;
   }
   return result;
 }
 
-void print (const Matrix &data) {
+void print(const Matrix &data) {
   for (int i = 0; i < data.size(); ++i) {
     for (int j = 0; j < data[i].size(); ++j) {
       cout << data[i][j].first;
@@ -59,8 +95,29 @@ void print (const Matrix &data) {
   }
 }
 
+void printDots(const Matrix &data) {
+  for (int i = 0; i < data.size(); ++i) {
+    for (int j = 0; j < data[i].size(); ++j) {
+      cout << data[i][j].first;
+      cout << (minRiskPathCoordinates.contains({j,i}) ? "█" : ".");
+    }
+    cout << endl;
+  }
+}
+
+void printSum(const Matrix &data) {
+  for (int i = 0; i < data.size(); ++i) {
+    for (int j = 0; j < data[i].size(); ++j) {
+      cout << data[i][j].second<< "\t";
+    }
+    cout << endl;
+  }
+}
+
+// This actually gets the path, works for test cases and part1 but not part 2.
+// The output path is different to the expected ones but good values. 
 void compute(Matrix &data, const Coordinate &goal, const Coordinate &coordinate,
-    unordered_map<Coordinate, int, util::pair_hash> &memo, vector<int> &path, int &sum) {
+    unordered_set<Coordinate, util::pair_hash> &memo, vector<int> &path, int &sum) {
   // cout << "Checking coordinate: "; util::printPair(coordinate, true);
   auto &cell = data[coordinate.second][coordinate.first];
   int value = cell.first;
@@ -71,47 +128,135 @@ void compute(Matrix &data, const Coordinate &goal, const Coordinate &coordinate,
   }
   path.push_back(value);
   sum += value;
+  memo.insert(coordinate);
   if (goal == coordinate) {
     ++totalPaths;
     if (sum < minRiskPathTotal) {
-      cout << "\tMin Risk Path found w/ sum: " << sum << endl;
+      cout << "\tMin Risk Path found w/ sum: " << sum << ". Size: " << path.size() << endl;
       minRiskPathTotal = sum;
       minRiskPath = path;
-      util::printVector(path);
+      minRiskPathCoordinates = memo;
+      // util::printVector(path);
       cout << endl;
     }
   } else {
     Coordinate aux; 
-    if (coordinate.first < data[coordinate.second].size() - 1) {  // Left
-      aux = {coordinate.first + 1, coordinate.second};
-      compute(data, goal, aux, memo, path, sum);
-    }
     if (coordinate.second < data.size() - 1) { // Bottom
       aux = {coordinate.first, coordinate.second + 1};
       compute(data, goal, aux, memo, path, sum);
     }
+    if (coordinate.first < data[coordinate.second].size() - 1) {  // Right
+      aux = {coordinate.first + 1, coordinate.second};
+      compute(data, goal, aux, memo, path, sum);
+    }
+    if (coordinate.first > 0) {  // Left
+      aux = {coordinate.first - 1, coordinate.second};
+      compute(data, goal, aux, memo, path, sum);
+    }
+    if (coordinate.second > 0) { // Top
+      aux = {coordinate.first, coordinate.second - 1};
+      compute(data, goal, aux, memo, path, sum);
+    }
   }
   sum -= value;
-  path.pop_back();
+  memo.erase(coordinate);
+  if (path.size()) path.pop_back();
 }
 
 int findPath(Matrix &data) {
   minRiskPathTotal = INT_MAX;
   Coordinate start(0, 0), goal(data.size() - 1, data.back().size() - 1);
-  unordered_map<Coordinate, int, util::pair_hash> memo;
+  unordered_set<Coordinate, util::pair_hash> memo;
   vector<int> path;
   int sum = 0;
   compute(data, goal, start, memo, path, sum);
   cout << "Total Paths: " << totalPaths << endl;
+  printDots(data);
   return minRiskPathTotal - data[0][0].first;
 }
 
+
+// Dijkstra-ish solution, visiting closer neighboors and keeping track of minimum cost.
+void compute2(Matrix &data, const Coordinate &goal, const Coordinate &coordinate,
+    int sum, deque<pair<int,Coordinate>> &nextCoordinates) {
+  if (coordinate.second >= data.size()
+     || coordinate.first >= data[coordinate.second].size()) {
+    return;
+  }
+
+  // If new path here is shorter than previously known, update. Else return.
+  auto &cell = getValue(data, coordinate);
+  int value = cell.first;
+  // sum += value;
+  if (sum < cell.second) {
+    cell.second = sum;
+    // cout << "\tUpdating: ";
+    // util::printPair(coordinate);
+    // cout << " Value: " << value << " Sum: " << sum << endl;
+  } else {
+    return;
+  }
+
+  if (goal == coordinate) {
+    if (sum < minRiskPathTotal) {
+      cout << "\tMin Risk Path found w/ sum: " << sum << endl;
+      minRiskPathTotal = sum;
+      cout << endl;
+    }
+  } else {
+    Coordinate aux; 
+    if (coordinate.second < data.size() - 1) { // Bottom
+      aux = {coordinate.first, coordinate.second + 1};
+      nextCoordinates.emplace_back(sum + getValue(data, aux).first, aux);
+    }
+    if (coordinate.first < data[coordinate.second].size() - 1) {  // Right
+      aux = {coordinate.first + 1, coordinate.second};
+      nextCoordinates.emplace_back(sum + getValue(data, aux).first, aux);
+    }
+    if (coordinate.first > 0) {  // Left
+      aux = {coordinate.first - 1, coordinate.second};
+      nextCoordinates.emplace_back(sum + getValue(data, aux).first, aux);
+    }
+    if (coordinate.second > 0) { // Top
+      aux = {coordinate.first, coordinate.second - 1};
+      nextCoordinates.emplace_back(sum + getValue(data, aux).first, aux);
+    }
+  }
+}
+
+int calculatePath(Matrix &data) {
+  minRiskPathTotal = INT_MAX;
+  Coordinate start(0, 0);
+  Coordinate goal(data.size() - 1, data.back().size() - 1);
+  Coordinate test(2, 2);
+  deque<pair<int,Coordinate>> nextCoordinates;
+  int sum = 0;
+  nextCoordinates.emplace_back(0, start);
+  while (nextCoordinates.size()) {
+    pair<int,Coordinate> p1 = nextCoordinates.front();
+    nextCoordinates.pop_front();
+    // cout << "Next coordinate w/ cost of: " << p1.first << ". "; util::printPair(p1.second);
+    // cout <<endl;
+    compute2(data, goal, p1.second, p1.first, nextCoordinates);
+    sort(nextCoordinates.begin(), nextCoordinates.end(),
+         [](const pair<int,Coordinate> &p1, const pair<int,Coordinate> &p2) {
+           return p1.first < p2.first;
+         });
+  }
+  // return minRiskPathTotal - data[0][0].first;
+  // return data[test.second][test.first].second;
+  return data[goal.second][goal.first].second;
+}
+
 void solve(int part = 1) {
-  size_t kTotalTurns = part == 1 ? 2020 : 30000000;
-  Matrix input = getInput();
+  Matrix input = getInput(part);
   print(input);
-  int result = findPath(input);
-  cout << "Part 1: Path with sum of: " << result  << " and size: " << minRiskPath.size() << "." << endl;
+  int result = calculatePath(input);
+  // printSum(input);
+  // util::printVector(minRiskPath);
+  cout << endl;
+  // printDots(input);
+  cout << endl << "Part 1: Path with sum of: " << result  << "." << endl;
   // util::printVector(minRiskPath);
 }
 
---
+++ AdventOfCode/2021/AdventOfCode_15.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+def get_desc():
+    return 15, 'Day 15: Chiton'
+
+def calc(log, values, mode, draw=False):
+    colors = {
+        1: (12, 7, 134),
+        2: (74, 2, 160),
+        3: (124, 2, 167),
+        4: (168, 34, 150),
+        5: (202, 70, 120),
+        6: (229, 107, 92),
+        7: (247, 147, 65),
+        8: (253, 195, 40),
+        9: (239, 248, 33),
+        'noted': (90, 90, 90),
+        'path': (255, 255, 255),
+    }
+    from grid import Grid
+    grid = Grid.from_text(values)
+
+    for xy in grid.grid:
+        grid.grid[xy] = int(grid.grid[xy])
+
+    if mode == 2:
+        w, h = grid.width(), grid.height()
+        pts = [(x, y, int(grid[x, y])) for x, y in grid.grid]
+        for ox, oy in [(x // 5, x % 5) for x in range(1, 25)]:
+            for x, y, val in pts:
+                grid[x + ox * w, y + oy * h] = ((val - 1 + ox + oy) % 9) + 1
+
+    todo = [((0, 0), 0, [(0, 0)])]
+    seen = set([(0, 0)])
+    cur_point = 0
+    frames = 300
+    # 249998 is how many cur_points a full run finds
+    next_show = [int(ease(x / frames) * 249998) for x in range(frames+1)]
+    while len(todo):
+        todo.sort(key=lambda x: x[1])
+        xy, cost, trail = todo.pop(0)
+        for oxy in grid.neighbors(xy, valid_only=True):
+            if oxy not in seen:
+                seen.add(oxy)
+                extra = grid[oxy]
+                grid.grid[oxy] = 'noted'
+                if draw:
+                    show = False
+                    if oxy == (grid.width() - 1, grid.height() - 1):
+                        show = True
+                    elif len(next_show) and cur_point >= next_show[0]:
+                        next_show.pop(0)
+                        show = True
+                    if show:
+                        log(f"Cur Point: {cur_point}, {len(next_show)} left.")
+                        temp = {}
+                        for xy in trail:
+                            temp[xy] = grid[xy]
+                            grid[xy] = 'path'
+                        grid.save_frame()
+                        for xy, val in temp.items():
+                            grid[xy] = val
+                    cur_point += 1
+                if oxy == (grid.width() - 1, grid.height() - 1):
+                    if draw:
+                        grid.draw_frames(color_map=colors, show_lines=False, cell_size=(1, 1))
+                    cost += extra
+                    return cost
+                todo.append((oxy, cost + extra, trail + [oxy]))
+
+def test(log):
+    values = log.decode_values("""
+        1163751742
+        1381373672
+        2136511328
+        3694931569
+        7463417111
+        1319128137
+        1359912421
+        3125421639
+        1293138521
+        2311944581
+    """)
+
+    log.test(calc(log, values, 1), 40)
+    log.test(calc(log, values, 2), 315)
+
+def run(log, values):
+    log(calc(log, values, 1))
+    log(calc(log, values, 2))
