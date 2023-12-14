91e59f124b481b3dab0f311e20ce1d5b4ab1f611
Michael Duarte <michael.duarte@hotmail.com>
2023-12-13 23:45:52 -0800

    2023/14 - Board rotation cycles + Cycle until inding start of a loop + simulate 1000000000 cycles

--- AdventOfCode/2023/AdventOfCode_14.h
+++ AdventOfCode/2023/AdventOfCode_14.h
@@ -28,57 +28,67 @@
 namespace aoc2023_14 {
 using namespace std;
 
+constexpr char kMovableRock = 'O';
+constexpr char kEmptySpace = '.';
+constexpr char kWall = '#';
+
 class PuzzleInput {
   public:
-  vector<string> map;
+  vector<string> map_str;
+  vector<vector<char>> map;
 
   static PuzzleInput GetInput() {
     PuzzleInput input;
     while(!cin.eof()) {
-      getline(cin, input.map.emplace_back());
+      getline(cin, input.map_str.emplace_back());
+      input.map.emplace_back();
+      for (char c: input.map_str.back()) {
+        input.map.back().emplace_back(c);
+      }
     }
     return input;
   }
 
+  bool operator==(const PuzzleInput& other) const {
+    for (int r = 0; r < map.size(); ++r) {
+      for (int c = 0; c < map.back().size(); ++c) {
+        if (map[r][c] != other.map[r][c]) {
+          return false;
+        }
+      }
+    }
+  return true;
+}
+
+  void print() const {
+    for (const auto& line : map) {
+      util::printVector(line);
+      cout << endl;
+    }
+    cout << endl;
+  }
+
   void ShakeNorth() {
-    cout << endl << "Shaking North" << endl << endl;
+    // cout << endl << "Shaking North" << endl << endl;
     for (int c = 0; c < map.back().size(); ++c) {
       int empty_spaces = 0;
       int first_empty_row = -1;
       for (int r = 0; r < map.size(); ++r) {
         switch(map[r][c]) {
-          case 'O': {
+          case kMovableRock: {
             if (!empty_spaces) {
               break;
             }
-            map[first_empty_row++][c] = 'O';
-            map[r][c] = '.';
-            --r; // let's reevaluate.
-            // int distance_to_move = r - first_empty_row;
-            // int movable_stones = 1;
-            // int r2 = r + 1;
-            // // how many stones we can move down?
-            // for (;r2 < map.size() && empty_spaces; ++r2, --empty_spaces) {
-            //   if (map[r2][c] != '0') {
-            //     break;
-            //   }
-            //   ++movable_stones;
-            // }
-            // // move stones down;
-            // int moved = std::min(empty_spaces, movable_stones);
-            // for (int i = first_empty_row; i < moved; ++i) {
-            //   map[r][c] = '0';
-            // }
-            // // reset rest of stones up;
-            // // for (;r2 )
+            map[first_empty_row++][c] = kMovableRock;
+            map[r][c] = kEmptySpace;
           }
           break;
-          case '.': 
+          case kEmptySpace: 
             if (empty_spaces++ == 0) {
               first_empty_row = r;
             }
             break;
-          case '#': 
+          case kWall: 
             first_empty_row = -1;
             empty_spaces = 0;
             break;
@@ -87,7 +97,7 @@ class PuzzleInput {
     }
   }
 
-  int64_t GetValue() {
+  int64_t GetValue() const {
     int64_t result = 0;
     for (int r = 0; r < map.size(); ++r) {
       int64_t row_result = 0;
@@ -96,19 +106,103 @@ class PuzzleInput {
           row_result += map.size() - r;
         }
       }
-      cout << "\tresult in row " << r << ", is: " << row_result << endl;
+      // cout << "\tresult in row " << r << ", is: " << row_result << endl;
       result += row_result;
     }
     return result;
   }
+
+  void ShakeForCycle(int cycles = 1) {
+    // struct input_hash {
+    //   std::size_t operator() (const PuzzleInput& input) const {
+    //     return input.GetValue();
+    //   }
+    // };
+    // unordered_set<PuzzleInput, input_hash> seen_values;
+    for (int c = 0; c < cycles; ++c) {
+      for (int i = 0; i < 4; ++i) {
+        ShakeNorth();
+        RotateClockwise();
+      }
+      // print();
+      // auto value = GetValue();
+      // auto it = seen_values.find(*this);
+      // if (it != seen_values.end()) {
+      //   cout <<"Found a repeated value \"" << value << "\" at cycle: " << c << endl;
+      //   print(); 
+      //   break;
+      // }
+      // seen_values.insert(*this);
+      // if ((c % 1000000) == 0) {
+      //   cout << "Cycle: " << c << ". Value: " << value << endl;
+      //   print(); 
+      // }
+    }
+  }
+
+  void RotateClockwise() {
+    int rowLimit = map.size() / 2;
+    int colLimit = map.back().size();
+    int dim = map.size() - 1;
+    for (int r = 0 ; r < rowLimit; ++r) {
+      --colLimit;
+      for (int c = r; c < colLimit; ++c){
+        char aux = map[r][c]; // copy top-left to aux
+        map[r][c] = map[dim-c][r]; // copy bottom-left to top-left
+        map[dim-c][r] = map[dim-r][dim-c]; // copy bottom-right to bottom-left 
+        map[dim-r][dim-c] = map[c][dim-r]; // copy top-right to bottom right
+        map[c][dim-r] = aux; // copy aux to top-right
+      }
+      // cout << "At Step: " << (r+1) << endl;
+      // printMatrix(matrix);
+    }
+  }
 };
+
+int64_t FindValueAtCycle(PuzzleInput& input, int64_t cycles = 1000000000) {
+  PuzzleInput input_2 = input;
+  int64_t c1 = 0;
+  input_2.ShakeForCycle();
+  int64_t c2 = 1;
+  while (c1 < cycles) {
+    if (input == input_2) {
+      cout << "Found matching inputs at cycle: " << c1 << endl;
+      int64_t pending = cycles - c1;
+      int64_t distance = c2 - c1;
+      int64_t addition = pending / distance * distance;
+      cout << "Pending: " << pending << ". Distance: " << distance << endl; 
+      cout << "Adding " << c1 << " += " << addition << endl; 
+      c1 += addition;
+      cout << "New cycle: " << c1 << endl; 
+      break;
+    }
+    input.ShakeForCycle();
+    ++c1;
+    input_2.ShakeForCycle(2);
+    c2 += 2;
+  }
+  while (c1 < cycles) {
+    input.ShakeForCycle();
+    ++c1;
+    cout << "Extra cycle: " << c1 << endl;
+  }
+  input.print();
+  return input.GetValue();
+}
 
 void solve(int part = 1) {
   PuzzleInput input = PuzzleInput::GetInput();
-  util::printVector(input.map, true);
-  input.ShakeNorth();
-  util::printVector(input.map, true);
-  cout << "Result: " << input.GetValue() << endl;
+  input.print();
+  int64_t result = 0;
+  if (part == 1) {
+    input.ShakeNorth();
+    result = input.GetValue();
+    input.print();
+  } else {
+    // input.ShakeForCycle(1000000000);
+    result = FindValueAtCycle(input);
+  }
+  cout << "Result: " << result << endl;
 
 }
 
