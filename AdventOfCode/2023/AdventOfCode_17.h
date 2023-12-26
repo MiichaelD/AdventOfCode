c6737d1bb2a32d6b369ede01b41f26053d34e7d8
Michael Duarte <michael.duarte@hotmail.com>
2023-12-25 19:25:40 -0800

    2023/17 - Shortest path but not optimal

--- AdventOfCode/2023/AdventOfCode_17.h
+++ AdventOfCode/2023/AdventOfCode_17.h
@@ -28,22 +28,185 @@
 namespace aoc2023_17 {
 using namespace std;
 
-void solve1() {
-  string input;
-  cin >> input;  
+constexpr size_t kMaxConsecutiveSteps = 3;
+constexpr char kNorth = 'N';
+constexpr char kSouth = 'S';
+constexpr char kEast = 'E';
+constexpr char kWest = 'W';
+const vector<char> kPosibleDirs{kNorth, kEast, kSouth, kWest};
+
+typedef unordered_map<pair<int,int>, int, util::pair_hash> VisitedPos;
+
+struct PosDirection {
+  PosDirection(const PosDirection& pos_dir)
+      : position(pos_dir.position), direction(pos_dir.direction),
+      rolling_value(pos_dir.rolling_value), consecutive(pos_dir.consecutive) {}
+  PosDirection(const pair<int,int>& pos, char d)
+      : position(pos), direction(d) {}
+  PosDirection(pair<int,int>&& pos, char d)
+      : position(std::move(pos)), direction(d) {}
+  int rolling_value = 0;
+  int consecutive = 0;
+  pair<int, int> position;
+  char direction;
+
+  void print() const {
+    util::printPair(position);
+    cout << ". Direction: " << direction << ". Rolling Value: " << rolling_value;
+    cout << ". Consecutive steps: " << consecutive << endl;
+  }
+
+  bool operator=(const PosDirection& pos_dir) {
+    return pos_dir.position.first == position.first
+        && pos_dir.position.second == position.second;
+  }
+};
+
+// Updates the current_pos 1 step into the given direction.
+inline void Advance(PosDirection& pos_dir) {
+  switch(pos_dir.direction) {
+    case kNorth: pos_dir.position.first -= 1; break;
+    case kSouth: pos_dir.position.first += 1; break;
+    case kWest: pos_dir.position.second -= 1; break;
+    case kEast: pos_dir.position.second += 1; break;
+  }
 }
 
-void solve2() {
-  string input;
-  cin >> input;
+bool IsOppositeDir(char dir1, char dir2) {
+  if (dir1 == kNorth && dir2 == kSouth) return true;
+  if (dir1 == kSouth && dir2 == kNorth) return true;
+  if (dir1 == kWest && dir2 == kEast) return true;
+  if (dir1 == kEast && dir2 == kWest) return true;
+  return false;
 }
+
+void AddPossibleDirections(
+    const vector<vector<int>>& map, const PosDirection& current, 
+    deque<PosDirection>& pos_dirs) {
+  for (char dir : kPosibleDirs) {
+    if (dir == current.direction && current.consecutive >= kMaxConsecutiveSteps) {
+      cout << "\tSkipping due consecutive direction: " << dir << endl;
+      continue;
+    }
+    if (IsOppositeDir(dir, current.direction)) {
+      cout << "\tSkipping due opposite direction: " << dir << endl;
+      continue;
+    }
+    PosDirection& new_pos = pos_dirs.emplace_back(current);
+    new_pos.direction = dir;
+    Advance(new_pos);
+    if (new_pos.position.first < 0 || new_pos.position.second < 0 ||
+        new_pos.position.first >= map.size() ||
+        new_pos.position.second >= map.back().size()) {
+      cout << "\tSkipping due Out of Bounds: " << dir << endl;
+      pos_dirs.pop_back();
+      continue;
+    }
+    new_pos.rolling_value += map[new_pos.position.first][new_pos.position.second];
+    if (dir == current.direction) {
+      ++new_pos.consecutive;
+    } else {
+      new_pos.consecutive = 1;
+    }
+    cout << "\tAdded: "; new_pos.print();
+  }
+}
+
+class PuzzleInput {
+  vector<vector<int>> map;
+  pair<int, int> start_pos{0, 0};
+  pair<int, int> target_pos;
+
+  void FindOptimalPath(
+      deque<PosDirection>& pos_dirs, VisitedPos& visited_pos, 
+      deque<PosDirection>& rolling_path, deque<deque<PosDirection>>& solutions) const {
+    int end_pos_val = INT_MAX;
+    // for (int i = 0; i < pos_dirs.size(); ++ i){
+    for (int i = 0; pos_dirs.size(); ++ i){
+      auto pos_dir = pos_dirs.front();
+      pos_dirs.pop_front();
+      cout << "Exploring: "; util::printPair(pos_dir.position);
+      cout << ". RollValue: " << pos_dir.rolling_value;
+      cout << ". MapVal: " << map[pos_dir.position.first][pos_dir.position.second] << endl;
+      auto it = visited_pos.find(pos_dir.position);
+      if (it != visited_pos.end() && it->second <= pos_dir.rolling_value) {
+        // We have seen this position and with a lower heat lost.
+        continue;
+      }
+      // Traverse from this point.
+      cout << "Marked as visited with new value: " << pos_dir.rolling_value << endl;
+      visited_pos[pos_dir.position] = pos_dir.rolling_value;
+      rolling_path.push_back(pos_dir);
+
+      if (pos_dir.position == target_pos &&
+          pos_dir.rolling_value <= end_pos_val) {  // We found a better path.
+        cout << "Found a path with value: " << pos_dir.rolling_value << endl;
+        solutions.push_back(rolling_path);
+        end_pos_val = pos_dir.rolling_value;
+        rolling_path.pop_back();  // Remove last added point.
+        continue;
+      }
+      AddPossibleDirections(map, pos_dir, pos_dirs);
+      rolling_path.pop_back();  // Remove last added point.
+    }
+    cout << "end_pos_val: " << end_pos_val << endl;
+  }
+
+ public:
+  static PuzzleInput GetInput(int part) {
+    PuzzleInput input;
+    std::string aux;
+    while(!cin.eof()) {
+      getline(cin, aux);
+      input.map.emplace_back();
+      for (char c : aux) {
+        input.map.back().emplace_back(c - '0');
+      }
+    }
+    input.target_pos =
+        std::make_pair(input.map.size() -1, input.map.back().size() -1);
+    return input;
+  }
+
+  void Print() const {
+    for (const auto& line : map) {
+      util::printVector(line);
+      cout << endl;
+    }
+  }
+
+  void Print(const VisitedPos& visited_pos) const {
+    for (int f = 0; f < map.size(); ++f) {
+      for (int c = 0; c < map.back().size(); ++c) {
+        cout << visited_pos.at(make_pair(f, c)) << ", ";
+      }
+      cout << endl;
+    }
+  }
+
+  size_t MinHeatLossPath() const {
+    deque<PosDirection> path;
+    VisitedPos visited_pos;
+    deque<deque<PosDirection>> solutions;
+    deque<PosDirection> pos_dirs{PosDirection(start_pos, 'E')};  
+    FindOptimalPath(pos_dirs, visited_pos, path, solutions);
+    for (const auto& solution : solutions) {
+      cout << "Solution: ";
+      for (const auto& pos_dir : solution) {
+        pos_dir.print();
+      }
+      cout << endl;
+    }
+    Print(visited_pos);
+    return visited_pos[target_pos];
+  }
+};
 
 void solve(int part = 1) {
-  if (part == 1) {
-    solve1();
-  } else {
-    solve2();
-  }
+  PuzzleInput input = PuzzleInput::GetInput(part);
+  input.Print();
+  auto heat_loss = input.MinHeatLossPath();
+  cout << endl << "Route with min heat lost of: " << heat_loss << endl;
 }
 
 };  // aoc2023_17
